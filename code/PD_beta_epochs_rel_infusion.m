bdab3c14e29ec004b8a7d5c748a0a504b77e2480
benp_mbp <benpolletta@gmail.com>
2014-06-17 16:44:22 -0400

    Changed scripts to look at beta segments based on striatal beta, motor beta, and *both*.

--- code/PD_beta_epochs_rel_infusion.m
+++ code/PD_beta_epochs_rel_infusion.m
@@ -1,4 +1,6 @@
-function PD_beta_epochs_rel_infusion(subject_mat, outlier_lim, sd_lim, win_size)
+function PD_beta_epochs_rel_infusion(subject_mat, outlier_lim, sd_lim, win_size, smooth_size)
+
+par_name = [num2str(outlier_lim),'out_',num2str(sd_lim),'sd_',num2str(win_size),'win_',num2str(smooth_size),'smooth'];
 
 load(subject_mat)
 
@@ -7,6 +9,16 @@ sampling_freq = 1000;
 pd_label = {'pre', 'post'};
 
 period_label = {'Pre-Infusion','Post-Infusion'};
+
+chan_labels = {chan_labels{:}, 'Both'};
+
+ch_index = {1, 2, 1:2};
+
+ch_label = {'ch1', 'ch2', 'ch1_ch2'};
+
+no_b_blocks = nan(length(folders), 3);
+
+no_dps = nan(length(folders), 3);
 
 for fo = 1:length(folders)
     
@@ -30,25 +42,33 @@ for fo = 1:length(folders)
     
     % beta_cutoff = mean(beta_amp(1:base_index, :)) + sd_lim*std(beta_amp(1:base_index, :));
     
-    figure;
-    
+    norm_data = nan(size(PD_dec));
+   
     for ch = 1:2
         
-        norm_data = (PD_dec(:, ch) - mean(PD_dec(:, ch)))/std(PD_dec(:, ch));
+        norm_data(:,ch) = (PD_dec(:, ch) - mean(PD_dec(:, ch)))/std(PD_dec(:, ch));
         
-        ba_flipped = [flipud(beta_amp(1:win_size, ch)); beta_amp(:,ch); flipud(beta_amp((end-win_size+1):end,ch))];
+        ba_flipped = [flipud(beta_amp(1:smooth_size, ch)); beta_amp(:,ch); flipud(beta_amp((end-smooth_size+1):end,ch))];
         
-        ba_conv = conv(ba_flipped, ones(win_size, 1)/win_size, 'same');
+        ba_conv = conv(ba_flipped, hann(smooth_size)/sum(hann(smooth_size)), 'same');
         
-        ba_smooth(:, ch) = ba_conv((win_size+1):(end-win_size));
+        ba_smooth(:, ch) = ba_conv((smooth_size+1):(end-smooth_size));
         
         beta_cutoff = mean(ba_smooth(1:base_index, ch)) + sd_lim*std(ba_smooth(1:base_index, ch));
         
         beta_high(:, ch) = ba_smooth(:, ch) >= beta_cutoff;
         
+    end
+    
+    beta_high(:,3) = sum(beta_high,2) >= 2;
+    
+    figure;
+    
+    for ch = 1:3
+        
         for pd = 1:size(pd_limits,1)
             
-            beta_listname = [subj_name,'_ch',num2str(ch),'_beta_',pd_label{pd},'.list'];
+            beta_listname = [subj_name,'_',ch_label{ch},'_beta_',pd_label{pd},'_',par_name,'.list'];
             
             fid_list = fopen(beta_listname, 'w');
             
@@ -56,14 +76,14 @@ for fo = 1:length(folders)
             
             fid_win_list = fopen([beta_listname(1:end-5), '_win.list'],'w');
             
-            subplot(2, 2, (ch-1)*2 + pd)
+            subplot(4, 2, (ch-1)*2 + pd)
             
-            % plot(t,beta_amp(:,ch),'k')
+            plot(t, beta_amp(:, ch_index{ch}),'k', t, ba_smooth(:, ch_index{ch}), 'b')
             
-            title([chan_labels{ch}, ' Beta Segments ', period_label{pd}])
+            % plot(t(pd_limits(pd,1):pd_limits(pd,2)), beta_amp(pd_limits(pd,1):pd_limits(pd,2), ch_index{ch}), 'k',...
+            %     t(pd_limits(pd,1):pd_limits(pd,2)), ba_smooth(pd_limits(pd,1):pd_limits(pd,2), ch_index{ch}), 'b')
             
-            plot(t(pd_limits(pd,1):pd_limits(pd,2)), beta_amp(pd_limits(pd,1):pd_limits(pd,2), ch), 'k',...
-                t(pd_limits(pd,1):pd_limits(pd,2)), ba_smooth(pd_limits(pd,1):pd_limits(pd,2), ch), 'b')
+            title([folder, ' ', chan_labels{ch}, ' Beta Segments ', period_label{pd}])
             
             axis tight
             
@@ -109,23 +129,23 @@ for fo = 1:length(folders)
                 
                 for b = 1:size(beta_blocks,1)
                         
-                    beta_name = [subj_name,'_ch',num2str(ch),'_beta_',pd_label{pd},'_block',num2str(b),'.txt'];
+                    beta_name = [subj_name,'_',par_name,'_ch',num2str(ch),'_beta_',pd_label{pd},'_block',num2str(b),'.txt'];
                     
-                    if any(abs(norm_data(beta_blocks(b,1):beta_blocks(b,2))) > outlier_lim)
+                    if any(abs(norm_data(beta_blocks(b,1):beta_blocks(b,2), ch_index{ch})) > outlier_lim)
                         
                         beta_out(index) = b;
                         
-                        plot(t(beta_blocks(b,1):beta_blocks(b,2)), beta_amp(beta_blocks(b,1):beta_blocks(b,2), ch), 'c')
+                        plot(t(beta_blocks(b,1):beta_blocks(b,2)), beta_amp(beta_blocks(b,1):beta_blocks(b,2), ch_index{ch}), 'c')
                         
-                        plot(t(beta_blocks(b,1):beta_blocks(b,2)), ba_smooth(beta_blocks(b,1):beta_blocks(b,2), ch), 'm')
+                        plot(t(beta_blocks(b,1):beta_blocks(b,2)), ba_smooth(beta_blocks(b,1):beta_blocks(b,2), ch_index{ch}), 'm')
                         
                         index = index + 1;
                         
                     else
                         
-                        plot(t(beta_blocks(b,1):beta_blocks(b,2)), beta_amp(beta_blocks(b,1):beta_blocks(b,2), ch), 'g')
+                        plot(t(beta_blocks(b,1):beta_blocks(b,2)), beta_amp(beta_blocks(b,1):beta_blocks(b,2), ch_index{ch}), 'g')
                         
-                        plot(t(beta_blocks(b,1):beta_blocks(b,2)), ba_smooth(beta_blocks(b,1):beta_blocks(b,2), ch), 'r')
+                        plot(t(beta_blocks(b,1):beta_blocks(b,2)), ba_smooth(beta_blocks(b,1):beta_blocks(b,2), ch_index{ch}), 'r')
                         
                         P_name = [beta_name(1:end-4),'_P.txt'];
                         
@@ -165,7 +185,13 @@ for fo = 1:length(folders)
                         
                         subplot(s_r, s_c, o)
                         
-                        plot(t(beta_blocks(bo,1):beta_blocks(bo,2)), norm_data(beta_blocks(bo,1):beta_blocks(bo,2)))
+                        plot(t(beta_blocks(bo,1):beta_blocks(bo,2)), norm_data(beta_blocks(bo,1):beta_blocks(bo,2), ch_index{ch}))
+                        
+                        if o == 1
+                           
+                            title([folder, ' ', chan_labels{ch}, ' Outlier Segments ', period_label{pd}])
+                            
+                        end
                         
                         axis tight
                         
@@ -175,7 +201,15 @@ for fo = 1:length(folders)
                     
                     close(gcf)
                     
+                    beta_blocks(beta_out, :) = [];
+                    
                 end
+                
+                no_b_blocks(fo, ch, pd) = size(beta_blocks, 1);
+                
+                beta_lengths = diff(beta_blocks, [], 2) + 1;
+                
+                no_dps(fo, ch, pd) = sum(beta_lengths);
                 
             end
             
@@ -183,6 +217,50 @@ for fo = 1:length(folders)
         
     end
     
-    save_as_pdf(gcf,[subj_name,'_beta_',pd_label{pd},'_',num2str(outlier_lim),'out_',num2str(sd_lim),'sd_',num2str(win_size),'win'])
+    subplot(4, 2, 7)
+    
+    bar(reshape(no_b_blocks(fo, :, :), 3, 2))
+    
+    title([folder, ' Number Beta Segments'])
+    
+    set(gca,'XTickLabel',chan_labels)
+    
+    legend(period_label)
+    
+    subplot(4, 2, 8)
+    
+    bar(reshape(no_dps(fo, :)/sampling_freq, 3, 2))
+    
+    title([folder, ' Length Beta Segments (s)'])
+    
+    set(gca,'XTickLabel',chan_labels)
+    
+    legend(period_label)
+    
+    save_as_pdf(gcf,[subj_name, '_beta_', par_name])
           
 end
+
+figure
+
+subplot(1, 2, 1)
+
+bar(reshape(sum(no_b_blocks), 3, 2))
+    
+title(['Total Number Beta Segments'])
+
+set(gca,'XTickLabel',chan_labels)
+
+legend(period_label)
+
+subplot(1, 2, 2)
+
+bar(reshape(sum(no_dps)/sampling_freq, 3, 2))
+
+title(['Total Length Beta Segments (s)'])
+
+set(gca,'XTickLabel',chan_labels)
+
+legend(period_label)
+
+save_as_pdf(gcf,[subject_mat(1:(end - length('_subject.mat'))), '_beta_', par_name])
--- code/PD_beta_epochs_rel_infusion_roseplot_by_datapoint.m
+++ code/PD_beta_epochs_rel_infusion_roseplot_by_datapoint.m
@@ -1,8 +1,12 @@
-function PD_beta_epochs_rel_infusion_roseplot_by_datapoint(subject_mat)
+function PD_beta_epochs_rel_infusion_roseplot_by_datapoint(subject_mat, outlier_lim, sd_lim, win_size, smooth_size)
+
+par_name = [num2str(outlier_lim),'out_',num2str(sd_lim),'sd_',num2str(win_size),'win_',num2str(smooth_size),'smooth'];
 
 load(subject_mat)
 
 sampling_freq = 1000;
+
+smooth_winsize = 50;
 
 f_bins = 8:4:32;
 
@@ -10,13 +14,17 @@ pd_label = {'pre','post'};
 
 period_label = {'Pre-Infusion','Post-Infusion'};
 
-fid_mat = nan(2,length(pd_label));
+chan_labels = {chan_labels{:}, 'Both'};
 
-for ch = 1:2
+ch_label = {'ch1', 'ch2', 'ch1_ch2'};
+
+fid_mat = nan(3,length(pd_label));
+
+for ch = 1:3
    
     for pd = 1:length(pd_label)
     
-        all_beta_name{ch, pd} = [subject_mat(1:(end-length('_subjects.mat'))),'_beta_ch',num2str(ch),'_',pd_label{pd}];
+        all_beta_name{ch, pd} = [subject_mat(1:(end-length('_subjects.mat'))),'_',par_name,'_beta_',ch_label{ch},'_',pd_label{pd}];
         
         fid_mat(ch, pd) = fopen([all_beta_name{ch, pd},'_pbf_dp.txt'], 'w');
     
@@ -28,9 +36,7 @@ for fo = 1:length(folders)
     
     folder = folders{fo};
     
-    prefix = prefixes{fo};
-    
-    basetime = basetimes(fo);    
+    prefix = prefixes{fo};  
     
     base_index = basetimes(fo)*sampling_freq;
     
@@ -42,15 +48,15 @@ for fo = 1:length(folders)
     
     no_periods = size(periods,1);
     
-    for ch = 1:2
+    for ch = 1:3
         
-        beta_name = [subj_name,'_ch',num2str(ch),'_beta'];
+        beta_name = [subj_name,'_',ch_label{ch},'_beta'];
         
         figure;
         
         for pd = 1:no_periods
             
-            beta_listname = [beta_name,'_',pd_label{pd},'.list'];
+            beta_listname = [beta_name,'_',pd_label{pd},'_',par_name,'.list'];
             
             % Loading block numbers, epoch start and end indices.
             [blocks, beta_starts, beta_ends] = text_read([beta_listname(1:end-5),'_win.list'],'%f%f%f%*[^\n]');
@@ -71,11 +77,11 @@ for fo = 1:length(folders)
                 
                 P_diff = angle(exp(sqrt(-1)*P_diff));
                 
-                Pd_flipped = [flipud(P_diff(1:100)); P_diff; flipud(P_diff((end-100+1):end))];
+                Pd_flipped = [flipud(P_diff(1:smooth_winsize)); P_diff; flipud(P_diff((end-smooth_winsize+1):end))];
                 
-                Pd_conv = conv(Pd_flipped,ones(100,1)/100,'same');
+                Pd_conv = conv(Pd_flipped,hann(smooth_winsize)/sum(hann(smooth_winsize)),'same');
                 
-                Pd_smooth = Pd_conv(101:(end-100));
+                Pd_smooth = Pd_conv((smooth_winsize + 1):(end - smooth_winsize));
                 
                 F = diff(P_block)/(2*pi*(1/sampling_freq));
                 
@@ -83,17 +89,18 @@ for fo = 1:length(folders)
                 
                 for ch1 = 1:2
                     
-                    F_flipped = [flipud(F(1:100,ch1)); F(:,ch1); flipud(F((end-100+1):end,ch1))];
+                    F_flipped = [flipud(F(1:smooth_winsize,ch1)); F(:,ch1); flipud(F((end-smooth_winsize+1):end,ch1))];
                     
-                    F_conv = conv(F_flipped,ones(100,1)/100,'same');
+                    % F_conv = conv(F_flipped,hann(smooth_winsize)/sum(hann(smooth_winsize)),'same');
+                    F_conv = conv(F_flipped,ones(smooth_winsize,1)/smooth_winsize,'same');
                     
-                    F_smooth(:,ch1) = F_conv(100 + (1:size(Pd_smooth,1)));
+                    F_smooth(:,ch1) = F_conv(smooth_winsize + (1:size(Pd_smooth,1)));
                     
                 end
                 
-                fprintf(fid, '%f\t%f\t%f\t%f\n', [b*ones(size(Pd_smooth)) F_smooth Pd_smooth]');
+                fprintf(fid, '%f\t%f\t%f\t%f\t%f\n', [b*ones(size(Pd_smooth)) F_smooth Pd_smooth]');
                 
-                fprintf(fid_mat(ch, pd), '%f\t%f\t%f\t%f\n', [b*ones(size(Pd_smooth)) F_smooth Pd_smooth]');
+                fprintf(fid_mat(ch, pd), '%f\t%f\t%f\t%f\t%f\n', [b*ones(size(Pd_smooth)) F_smooth Pd_smooth]');
                 
             end
             
@@ -111,7 +118,7 @@ for fo = 1:length(folders)
                 
                 all_Fs = [nan nan];
                 
-                all_Pds = [nan];
+                all_Pds = nan;
                 
             end
             
@@ -135,7 +142,7 @@ end
 
 close('all')
 
-for ch = 1:2
+for ch = 1:3
     
     for pd = 1:length(pd_label)
         
@@ -149,11 +156,13 @@ figure(1)
 
 index = 2;
 
-for ch = 1:2
+for ch = 1:3
            
     for ch1 = 1:2
         
         figure(index)
+        
+        ch_Fs = [];
         
         for pd = 1:length(pd_label)
             
@@ -161,7 +170,11 @@ for ch = 1:2
             
             all_Fs = all_beta_data(:,2:3);
             
+            ch_Fs(end + 1:size(all_Fs,1),:) = all_Fs;
+            
             all_Pds = all_beta_data(:,4);
+            
+            ch_Pds(end + 1:size(all_Fs,1),:) = all_Fs;
             
             figure(index)
             
@@ -173,7 +186,7 @@ for ch = 1:2
             
             figure(1)
             
-            subplot(2, 4, (ch-1)*(2 + length(pd_label)) + (pd-1)*2 + ch1)
+            subplot(3, 4, (ch-1)*(2 + length(pd_label)) + (pd-1)*2 + ch1)
             
             rose_plot(all_Pds, all_Fs(:,ch1), 20, f_bins);
             
@@ -181,7 +194,7 @@ for ch = 1:2
             
         end
         
-        save_as_pdf(index, [subject_mat(1:(end-length('_subjects.mat'))),'_ch',num2str(ch),'_by_ch',num2str(ch1),'_beta_ri_rose_dp'])
+        save_as_pdf(index, [subject_mat(1:(end-length('_subjects.mat'))),'_',par_name,'_',ch_label{ch},'_by_ch',num2str(ch1),'_beta_ri_rose_dp'])
         
         index = index + 1;
         
@@ -189,5 +202,28 @@ for ch = 1:2
     
 end
 
+save_as_pdf(gcf,[subject_mat(1:(end-length('_subjects.mat'))),'_',par_name,'_beta_ri_rose_dp'])
 
-save_as_pdf(gcf,[subject_mat(1:(end-length('_subjects.mat'))),'_beta_ri_rose_dp'])
+end
+
+function F_c = categorize_freq(F, f_bins)
+    
+    [r, c] = size(F);
+    
+    F_c = zeros(r, c);
+    
+    no_f_bins = length(f_bins) - 1;
+    
+    for col = 1:c
+        
+        for f = 1:no_f_bins
+            
+            F_bin = F(:, col) >= f_bins(f) & F(:, col) < f_bins(f + 1);
+            
+            F_c = F_c + f*F_bin;
+            
+        end
+        
+    end
+    
+end
