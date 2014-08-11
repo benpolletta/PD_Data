95b1cf6c86f541ec0bdde4869221299480a1ed49
benp_mbp <benpolletta@gmail.com>
2014-08-11 16:25:40 -0400

    Script for running beta epochs - by individual and group - all with one command.

---
+++ code/run_PD_beta_epochs.m
@@ -0,0 +1,13 @@
+function run_PD_beta_epochs(subject_mat, outlier_lim, sd_lim, win_size, smooth_size)
+
+close('all')
+
+PD_beta_epochs_rel_infusion_roseplot_by_datapoint_group(subject_mat, outlier_lim, sd_lim, win_size, smooth_size)
+
+close('all')
+
+PD_beta_epochs_rel_infusion_roseplot_by_datapoint_individual(subject_mat, outlier_lim, sd_lim, win_size, smooth_size)
+
+close('all')
+
+PD_beta_epochs_rel_infusion_roseplot_by_dp_ind_avg(subject_mat, outlier_lim, sd_lim, win_size, smooth_size)
