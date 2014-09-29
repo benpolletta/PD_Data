fb3f038b5e4e5b40706c7eb1c74ba36b19d108af
bp_@_scc2 <benpolletta@gmail.com>
2014-09-29 14:57:14 -0400

    File to run all coherence at once.

---
+++ code/run_PD_coherence.m
@@ -0,0 +1,21 @@
+function run_PD_coherence(subject_mat, outlier_lim, sd_lim, win_size, smooth_size, tbw)
+
+PD_beta_epochs_rel_infusion(subject_mat, outlier_lim, sd_lim, win_size, smooth_size)
+
+close('all')
+
+PD_beta_epochs_coh_mtm(subject_mat, outlier_lim, sd_lim, win_size, smooth_size, tbw)
+
+close('all')
+
+PD_beta_epochs_coh_mtm_collect(subject_mat, outlier_lim, sd_lim, win_size, smooth_size, tbw)
+
+close('all')
+
+PD_beta_epochs_coh_mtm_plot_group(subject_mat, outlier_lim, sd_lim, win_size, smooth_size, tbw)
+
+close('all')
+
+% PD_beta_epochs_coh_mtm_plot_individual(subject_mat, outlier_lim, sd_lim, win_size, smooth_size, tbw)
+% 
+% close('all')
