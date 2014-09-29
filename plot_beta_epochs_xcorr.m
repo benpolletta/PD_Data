f81e9f83c8e42a021b73e004279b3f6a8e84a66d
bp_@_scc2 <benpolletta@gmail.com>
2014-09-29 14:56:55 -0400

    File for batch plotting.

---
+++ plot_beta_epochs_xcorr.m
@@ -0,0 +1,5 @@
+run /project/crc-nak/brpp/startup
+
+PD_beta_epochs_xcorr_plot_individual('st_m1_subjects.mat',7,2,333,20000)
+
+PD_beta_epochs_xcorr_plot_individual('st_stn_subjects.mat',7,2,333,20000)
