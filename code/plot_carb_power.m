b102e3402003ec7be6cc2f2fc3208f32de3ebb8e
benp <benpolletta@gmail.com>
2015-07-27 13:20:37 -0400

    Revert "Removing parallelism from collect_carb_power and plot_carb_power." (Wanted to remove parallelism from scripts for optogenetics data, not carbachol data.)
    This reverts commit 56b6fc99c5a73f2437e2bccd261dd32733d03a46.

--- code/plot_carb_power.m
+++ code/plot_carb_power.m
@@ -4,16 +4,16 @@ matnames = {'st_m1', 'st_stn', 'st_m1_6OHDA'};
 
 period_labels = {'_by_STR', '_by_STR', ''};
 
-% matlabpool open 3
+matlabpool open 3
 
-for i = 1:3
+parfor i = 1:3
 
     PD_beta_blocks_rel_infusion_pre_post_plot_individual([matnames{i}, '_subjects.mat'],...
         150, 'ranksum', [period_labels{i}, norm, '_power'], freqs, no_cycles, bands)
     
 end
 
-% matlabpool close
+matlabpool close
 
 PD_beta_blocks_rel_infusion_pre_post_plot_individual('STR_subjects.mat',...
     150, 'ranksum', [norm, '_power'], freqs, no_cycles, bands)
