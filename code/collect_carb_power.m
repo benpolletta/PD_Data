56b6fc99c5a73f2437e2bccd261dd32733d03a46
benp <benpolletta@gmail.com>
2015-07-27 13:15:49 -0400

    Removing parallelism from collect_carb_power and plot_carb_power.

--- code/collect_carb_power.m
+++ code/collect_carb_power.m
@@ -4,16 +4,16 @@ matnames = {'st_m1', 'st_stn', 'st_m1_6OHDA'};
 
 period_labels = {'_by_STR', '_by_STR', ''};
 
-matlabpool open 3
+% matlabpool open 3
 
-parfor i = 1:3
+for i = 1:3
 
     PD_beta_blocks_rel_infusion_pre_post_power([matnames{i}, '_subjects.mat'],...
         150, period_labels{i}, norm, freqs, no_cycles, bands)
     
 end
 
-matlabpool close
+% matlabpool close
 
 collect_striatal_power_density(150, '_power', norm, freqs, no_cycles, bands)
 
