9f60732830467a77c0fb2fbbd9a7dd5b10a6ff27
benpolletta <benpolletta@gmail.com>
2015-09-01 18:14:55 -0400

    Debugging white lines to delineate beta.

--- code/make_spectrogram_figures.m
+++ code/make_spectrogram_figures.m
@@ -11,17 +11,17 @@ load([subject_name, '/', subject_name([1 2 4:end]), '_peaks.mat'])
 
 for ch = 1:2
     
-    figure
+    % figure
     
     %% Plotting spectrogram for whole data recording.
     
-    Str_spec = abs(Spec(:, :, ch));
+    Ch_spec = abs(Spec(:, :, ch));
     
-    dims = size(Str_spec);
+    dims = size(Ch_spec);
     
     dec_factor = 1; s_rate = 500/dec_factor;
     
-    Spec_dec = nanmean(reshape(Str_spec', dims(2), dec_factor, dims(1)/dec_factor), 2);
+    Spec_dec = nanmean(reshape(Ch_spec', dims(2), dec_factor, dims(1)/dec_factor), 2);
     Spec_dec = permute(Spec_dec, [1 3 2]);
     
     t = ((1:length(Spec_dec))/s_rate - basetime)/60;
@@ -29,7 +29,7 @@ for ch = 1:2
     Spec_dec = Spec_dec(1:80, t <= 30);
     t(t > 30) = [];
     
-    subplot(4, 1, 1)
+    figure, subplot(2, 1, 1) % subplot(4, 1, 1)
     
     imagesc(t, 1:80, Spec_dec(1:80, :))
     
@@ -68,7 +68,7 @@ for ch = 1:2
     
     %% Plotting LFP for whole data recording.
     
-    subplot(4, 1, 2)
+    subplot(2, 1, 2) % subplot(4, 1, 2)
     
     plot(t, PD_dec(t <= 30, ch), 'k')
     
@@ -79,6 +79,16 @@ for ch = 1:2
     xlabel('Time (min.) Rel. Infusion')
     
     ylabel('LFP (mV)')
+    
+    try
+        
+        save_as_eps(gcf, [subject_name, '/', subject_name, '_spec_for_paper_ch', num2str(ch)])
+        
+    catch
+        
+        save(gcf, [subject_name, '/', subject_name, '_spec_for_paper_ch', num2str(ch), '.fig'])
+        
+    end
     
     %% Plotting spectrogram & LFP for 10 seconds.
     
@@ -94,9 +104,9 @@ for ch = 1:2
         
         t_interval = t(t_index)*60;
         
-        subplot(4, 2, 4 + pd)
+        figure, subplot(2, 1, 1) % subplot(4, 2, 4 + pd)
         
-        imagesc(t_interval, 1:80, Str_spec(t_index, 1:80)')
+        imagesc(t_interval, 1:80, Ch_spec(t_index, 1:80)')
         
         title(pd_labels{pd})
         
@@ -107,12 +117,6 @@ for ch = 1:2
         axis xy
         
         hold on
-        
-        plot([0 0], [0 80], ':w', 'LineWidth', 2)
-        
-        plot([t_interval(1) t_interval(end)]*60, [30 30], ':w', 'LineWidth', 2)
-        
-        plot([t_interval(1) t_interval(end)]*60, [8 8], ':w', 'LineWidth', 2)
         
         if sum(Spike_indicator(t_index, ch)) > 0
             
@@ -130,7 +134,11 @@ for ch = 1:2
             
         end
         
-        subplot(4, 2, 6 + pd)
+        plot([t_interval(1) t_interval(end)], [30 30], ':w', 'LineWidth', 2)
+        
+        plot([t_interval(1) t_interval(end)], [8 8], ':w', 'LineWidth', 2)
+        
+        subplot(2, 1, 2) % subplot(4, 2, 6 + pd)
         
         plot(t_interval, PD_dec(t_index, ch), 'k')
         
@@ -150,15 +158,15 @@ for ch = 1:2
         
         ylabel('LFP (mV)')
         
-    end
-    
-    try
-        
-        save_as_eps(gcf, [subject_name, '/', subject_name, '_spec_for_paper_ch', num2str(ch)])
-        
-    catch
-        
-        save(gcf, [subject_name, '/', subject_name, '_spec_for_paper_ch', num2str(ch), '.fig'])
+        try
+            
+            save_as_eps(gcf, [subject_name, '/', subject_name, '_spec_for_paper_ch', num2str(ch), '_', pd_labels{pd}])
+            
+        catch
+            
+            save(gcf, [subject_name, '/', subject_name, '_spec_for_paper_ch', num2str(ch), '_', pd_labels{pd} '.fig'])
+            
+        end
         
     end
     
