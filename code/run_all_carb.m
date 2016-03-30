55ebf5dabe3a91518a8944eb7f78dafc7afa54c3
benpolletta <benpolletta@gmail.com>
2016-03-30 11:56:23 -0400

    Moving phase analysis to the end.

--- code/run_all_carb.m
+++ code/run_all_carb.m
@@ -8,12 +8,6 @@ for m = 1:no_mats
 
     run_carb([subject_mat_prefixes{m}, '_subjects.mat'], peak_suffix, freqs, no_cycles, bands)
 
-    for ftol = 2:3
-        
-        run_phase_analysis([subject_mat_prefixes{m}, '_subjects.mat'], peak_suffix, 200, .5, ftol, freqs, no_cycles, bands, band_index, 1, 150)
-        
-    end
-
 end
 
 measures = {'', '_power'}; norms = {'', '_pct'};
@@ -24,8 +18,6 @@ for m = 1:length(measures)
     
     PD_beta_blocks_rel_infusion_pre_post_plot('STR_M1_subjects.mat', peak_suffix, 150, [norms{m}, measures{m}], 't-test', freqs, no_cycles, bands)
 
-    PD_beta_blocks_rel_infusion_pre_post_plot_individual(
-
 end
    
 collect_striatal_w_motor_spectrum(peak_suffix, 150, '_pct', 3, freqs, no_cycles, bands)
@@ -35,3 +27,15 @@ PD_beta_blocks_rel_infusion_pre_post_spectrum_plot('STR_w_M1_subjects.mat', peak
 collect_motor_spectrum(peak_suffix, 150, '_pct', 3, freqs, no_cycles, bands)
 
 PD_beta_blocks_rel_infusion_pre_post_spectrum_plot('M1_subjects.mat', peak_suffix, 150, '_power', '_pct', band_index, 6, freqs, no_cycles, bands)
+
+for m = 1:no_mats
+
+    for ftol = 2:3
+        
+        run_phase_analysis([subject_mat_prefixes{m}, '_subjects.mat'], peak_suffix, 200, .5, ftol, freqs, no_cycles, bands, band_index, 1, 150)
+        
+    end
+
+end
+
+collect_striatal_motor_freqs(peak_suffix, 200, .5, '', band_index, freqs, no_cycles, bands)
