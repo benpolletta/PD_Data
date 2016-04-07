15feca17f2713d2191b3bcd1945cd661b68b920b
benpolletta <benpolletta@gmail.com>
2016-04-07 16:12:31 -0400

    Adding PLV analysis to run_carb.

--- code/run_carb.m
+++ code/run_carb.m
@@ -44,4 +44,8 @@ for i = 1:length(power_handles)
     
 end
 
+PD_beta_blocks_rel_infusion_pre_post_PLV(subject_mat, peak_suffix, 150, '_by_STR', band_index, freqs, no_cycles, bands)
+
+PD_beta_blocks_rel_infusion_pre_post_PLV_plot(subject_mat, peak_suffix, 150, '_by_STR', 4, 7, freqs, no_cycles, bands)
+
 PD_rel_infusion_plot_spectrogram(subject_mat, peak_suffix, 2, 20)
