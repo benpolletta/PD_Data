c22430e6761df47cfe572656d6b6b05b271bdce1
benpolletta <benpolletta@gmail.com>
2016-04-20 14:34:08 -0400

    Replacing phase analysis w/ PLV analysis in run_all_carb.

--- code/run_all_carb.m
+++ code/run_all_carb.m
@@ -42,17 +42,21 @@ PD_beta_blocks_rel_infusion_pre_post_spectrum_plot('M1_subjects.mat', peak_suffi
 
 make_spectra_figures('M1', peak_suffix)
 
-for m = 1:no_mats
-        
-    run_phase_analysis([subject_mat_prefixes{m}, '_subjects.mat'], peak_suffix, 200, .5, 2, freqs, no_cycles, bands, band_index, 1, 150)
+collect_striatal_w_motor_PLV(peak_suffix, 150, band_index, freqs, no_cycles, bands)
 
-end
+make_PLV_figures('STR_M1', peak_suffix)
 
-collect_striatal_motor_freqs(peak_suffix, 200, .5, '', band_index, freqs, no_cycles, bands)
-
-for ftol = 2:3
-    
-    beta_blocks_consolidated_phase_analysis('STR_M1_subjects.mat', peak_suffix, 200, .5, '', band_index, ftol, freqs, no_cycles, bands)
-    
-end
+% for m = 1:no_mats
+%         
+%     run_phase_analysis([subject_mat_prefixes{m}, '_subjects.mat'], peak_suffix, 200, .5, 2, freqs, no_cycles, bands, band_index, 1, 150)
+% 
+% end
+% 
+% collect_striatal_motor_freqs(peak_suffix, 200, .5, '', band_index, freqs, no_cycles, bands)
+% 
+% for ftol = 2:3
+%     
+%     beta_blocks_consolidated_phase_analysis('STR_M1_subjects.mat', peak_suffix, 200, .5, '', band_index, ftol, freqs, no_cycles, bands)
+%     
+% end
 
