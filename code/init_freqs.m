420e8efae07379042aa07bf403cf7ca24618041a
benpolletta <benpolletta@gmail.com>
2017-02-03 17:33:02 -0500

    Script to finally replace the endlessly cut and pasted code producing 'BP_suffix' and replacing empty bands with default values.

---
+++ code/init_freqs.m
@@ -0,0 +1,19 @@
+function [freqs, no_cycles, bands, BP_suffix] = init_freqs(freqs, no_cycles, bands)
+
+if isempty(freqs) && isempty(no_cycles) && isempty(bands)
+    
+    freqs = 1:200;
+    
+    no_cycles = linspace(3, 21, length(freqs));
+    
+    bands = [1 4; 4 8; 8 30; 30 100; 120 180; 0 200];
+    
+    BP_suffix = ['', peak_suffix];
+    
+else
+    
+    BP_suffix = sprintf('_%.0f-%.0fHz_%.0f-%.0fcycles_%dbands', freqs(1), freqs(end), no_cycles(1), no_cycles(end), size(bands, 1));
+    
+    BP_suffix = [BP_suffix, peak_suffix];
+    
+end
