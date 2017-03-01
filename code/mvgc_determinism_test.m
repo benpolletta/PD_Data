33f0b3ae2633223dd0bfdc7bd5d98c4ff3abae78
benpolletta <benpolletta@gmail.com>
2017-03-01 12:57:33 -0500

    Separating analysis from plotting for mvgc determinism test.

--- code/mvgc_determinism_test.m
+++ code/mvgc_determinism_test.m
@@ -16,12 +16,6 @@ subjects_struct = load(subjects_mat_name);
 
 striatal_channel = find(strcmp(subjects_struct.chan_labels, 'Striatum'));
 
-f = nan(75002, 2, 2, number_iterations, 2);
-
-moAIC = nan(number_iterations, 2);
-
-info = cell(number_iterations, 2);
-
 figure
 
 for fo = 1:length(subjects_struct.folders)
@@ -49,6 +43,12 @@ for fo = 1:length(subjects_struct.folders)
     end
     
     t = (1:size(data, 1))/data_labels_struct.sampling_freq{1} - subjects_struct.basetimes(fo); % t = t/60;
+    
+    f = nan(75002, 2, 2, number_iterations, 2);
+    
+    moAIC = nan(number_iterations, 2);
+    
+    info = cell(number_iterations, 2);
     
     for pd = 1:2
         
@@ -78,36 +78,6 @@ for fo = 1:length(subjects_struct.folders)
         
     end
     
-    save([folder, 'mvgc_determinism_test'], 'f', 'moAIC', 'info')
-    
-    f = permute(f, [1 4 2 3 5]);
-    
-    frequencies = 500*(1:size(f, 1))/size(f,1);
-    
-    dir_channels = {1, 2};
-    
-    for d = 1:2
-        
-        dir_channels = fliplr(dir_channels);
-        
-        subplot(length(subjects_struct.folders), 2, (fo - 1)*2 + d)
-        
-        % subplot_index = (p - 1)*2 + d;
-        %
-        % subplot(2, 2, subplot_index)
-        
-        f_mean = squeeze(nanmean(f(:, :, dir_channels{:}, :), 2));
-        
-        f_std = squeeze(nanstd(f(:, :, dir_channels{:}, :), [], 2));
-        
-        boundedline(frequencies, f_mean, prep_for_boundedline(f_std))
-        
-        box off
-        
-        ylabel(folder)
-        
-    end
+    save([subj_name, '_mvgc_determinism_test'], 'f', 'moAIC', 'info')
     
 end
-
-save_as_pdf(gcf, 'mvgc_determinism_test')
