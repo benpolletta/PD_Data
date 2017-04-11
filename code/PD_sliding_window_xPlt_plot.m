177f572147b93f83e2ed605f6560ff126a49f697
benpolletta <benpolletta@gmail.com>
2017-04-11 14:23:52 -0400

    PD_sliding_window_xPlt_plot: adding possibility of executing several normalizations; unpacking shuffles and renaming the axis 'Window_Dim_1'; replacing switch-case normalization with function call; packing 'Recording' axis into dimension after last non-singleton dimension; etc.

--- code/PD_sliding_window_xPlt_plot.m
+++ code/PD_sliding_window_xPlt_plot.m
@@ -1,4 +1,4 @@
-function PD_sliding_window_xPlt_plot(function_name, sliding_window_cell, data_labels_struct, pd_names, filename, significance, norm, varargin)
+function PD_sliding_window_xPlt_plot(function_name, sliding_window_cell, data_labels_struct, filename, pd_names, significance, norm, varargin)
     
 % Loads sliding window analysis on carbachol data at times of highest
 % striatal beta band density, pre- and post-infusion.
@@ -30,111 +30,93 @@ for period = 1:no_periods
     
 end
 
+if iscellstr(norm)
+    
+    norm_label = '';
+    
+    for n = 1:length(norm)
+        
+        norm_label = [norm_label, '_', norm{n}];
+        
+    end
+    
+elseif ischar(norm)
+    
+    norm_label = norm;
+    
+end
+
 load([make_sliding_window_analysis_name([filename, pd_label,...
     '_band', num2str(data_labels_struct.band_index)], function_name,...
     window_time_cell, 2, varargin{:}), '_xPlt.mat'])
 
-SW_xPlt = xp_abs(SW_xPlt);
-
-SW_xPlt.getaxisinfo
-
-frequencies = SW_xPlt.meta.matrix_dim_1.values;
-
-%% Normalizing.
-
-switch norm
+if ~strcmp(function_name, 'PAC')
     
-    case 'frequency'
-        
-        freq_mult = diag(frequencies.^(2/3));
-        
-        SW_xPlt.data = cellfun(@(x) freq_mult*x, SW_xPlt.data, 'UniformOutput', 0);
-        
-    case 'baseline'
-
-        SW_Baseline = load([make_sliding_window_analysis_name([filename, '_baseline_band',...
-            num2str(data_labels_struct.band_index)], function_name,...
-            window_time_cell, 2, varargin{:}), '_xPlt.mat']);
-        
-        SW_Baseline = xp_abs(SW_Baseline.SW_xPlt);
-        
-        if ~isempty(SW_Baseline.findaxis('Window_Dim_1'))
-            
-            SW_Baseline = mean_over_axis(SW_Baseline, 'Window_Dim_1');
-            
-        end
-        
-        % SW_Baseline = SW_Baseline.repmat(SW_xPlt.axis(SW_xPlt.findaxis('Period')).values, 'Period');
-        
-        if ~isempty(SW_xPlt.findaxis('Window_Dim_1'))
-            
-            SW_Baseline = SW_Baseline.repmat(SW_xPlt.axis(SW_xPlt.findaxis('Window_Dim_1')).values, 'Window_Dim_1', SW_xPlt.findaxis('Window_Dim_1'));
-            
-        end
-        
-        SW_Baseline = SW_Baseline.unpackDim(2, SW_xPlt.findaxis('Period'), 'Period', {'baseline'});
-        
-        SW_Baseline.getaxisinfo
-        
-        SW_BaselineMerged = SW_xPlt.merge(SW_Baseline);
-        
-        SW_BaselineMerged.getaxisinfo
-        
-        SW_xPlt = norm_axis_by_value(SW_BaselineMerged, 'Period', 'baseline');
-        
-        SW_xPlt = SW_xPlt.axissubset('Period', 'p');
-        
-        SW_xPlt.getaxisinfo
-        
-        % SW_xPlt.data = cellfun(@(x, y) 100*(x./y - 1), SW_xPlt.data, SW_Baseline.data, 'UniformOutput', 0);
-        
-    case 'totalpower'
-        
-        SW_xPlt.data = cellfun(@(x) x/sum(x), SW_xPlt.data, 'UniformOutput', 0);
-        
-    case ''
-            
-end
-
-%% Downsampling and restricting frequency.
-
-if ~isempty(SW_xPlt.findaxis('Window_Dim_1')) && all_dimensions(@any, cellfun(@isempty, SW_xPlt.data))
+    SW_xPlt = xp_abs(SW_xPlt);
     
-    SW_xPlt = SW_xPlt.packDim('Window_Dim_1');
+    frequencies = SW_xPlt.meta.matrix_dim_1.values;
     
-    SW_xPlt = squeeze(SW_xPlt.unpackDim(2));
+else
+    
+    amp_freqs = SW_xPlt.meta.matrix_dim_1.values;
+    
+    phase_freqs = SW_xPlt.meta.matrix_dim_2.values;
     
 end
 
-if isempty(SW_xPlt.findaxis('Period'))
-   
-    SW_xPlt = repmat(squeeze(SW_xPlt), {'baseline1', 'baseline2'}, 'Period');
+if ~isempty(regexp(pd_label, 'shuffle', 'once'))
     
-end
-
-if sliding_window_cell{1}(1) > 1000
-    
-    ds_factor = sliding_window_cell{1}(1)/1000;
-    
-    ds_factors = factor(ds_factor);
-    
-    for f = 1:length(ds_factors)
-
-        SW_xPlt.data = cellfun(@(x) empty_decimate(x, ds_factors(f)), SW_xPlt.data, 'UniformOutput', 0);
+    if isempty(SW_xPlt.findaxis('Shuffles'))
         
-        frequencies = decimate(frequencies, ds_factors(f));
+        SW_xPlt = SW_xPlt.unpackDim(axes_info_struct.odims + 1);
         
     end
     
+    shuffle_axis = SW_xPlt.findaxis('Shuffles');
+    
+    SW_xPlt.axis(shuffle_axis).name = 'Window_Dim_1';
+    
 end
 
-freq_limit = 50; freq_indicator = frequencies <= freq_limit;
+%% Normalizing.
 
-frequencies = frequencies(freq_indicator);
+for n = 1:length(norm)
 
-SW_xPlt.meta.matrix_dim_1.values = frequencies;
+    SW_xPlt = PD_sliding_window_normalize(SW_xPlt, function_name, sliding_window_cell, data_labels_struct, filename, pd_names, norm{n}, varargin{:});
+    
+end
 
-SW_xPlt.data = cellfun(@(x) x(freq_indicator), SW_xPlt.data, 'UniformOutput', 0);
+size_dim2 = cellfun(@(x) size(x, 2), SW_xPlt.data);
+    
+if ~any(size_dim2(:) > 1)
+    
+    %% Downsampling and restricting frequency.
+    
+    if sliding_window_cell{1}(1) > 1000 && isint(sliding_window_cell{1}(1)/1000)
+        
+        ds_factor = sliding_window_cell{1}(1)/1000;
+        
+        ds_factors = factor(ds_factor);
+        
+        for f = 1:length(ds_factors)
+            
+            SW_xPlt.data = cellfun(@(x) decimate(x, ds_factors(f)), SW_xPlt.data, 'UniformOutput', 0);
+            
+            frequencies = decimate(frequencies, ds_factors(f));
+            
+        end
+        
+    end
+    
+    freq_limit = 50; freq_indicator = frequencies <= freq_limit;
+    
+    frequencies = frequencies(freq_indicator);
+    
+    SW_xPlt.meta.matrix_dim_1.values = frequencies;
+    
+    SW_xPlt.data = cellfun(@(x) x(freq_indicator), SW_xPlt.data, 'UniformOutput', 0);
+    
+end
 
 %% Loading & looping over groups.
 
@@ -150,110 +132,204 @@ for group = 1:length(groups_plotted)
     
     group_name = [make_sliding_window_analysis_name([filename, groups_plotted{group}{1}, pd_label,...
     '_band', num2str(data_labels_struct.band_index)], function_name,...
-    window_time_cell, 2, varargin{:}), norm];
+    window_time_cell, 2, varargin{:}), norm_label];
+  
+    recording_pack_dim = SW_xPlt.lastNonSingletonDim + 1;
+
+    SW_group = SW_xPlt.packDim('Recording', recording_pack_dim);
     
-    SW_group = SW_xPlt.packDim('Recording', 2);
+    folders = SW_group.meta.(['matrix_dim_', num2str(recording_pack_dim)]).values;
     
-    folders = SW_group.meta.matrix_dim_2.values;
+    indices = cell(1, recording_pack_dim); indices(:) = {':'}; indices(recording_pack_dim) = groups_plotted{group}(2);
     
-    SW_group.data = cellfun(@(x) x(:, groups_plotted{group}{2}), SW_group.data, 'UniformOutput', 0);
+    SW_group.data = cellfun(@(x) x(indices{:}), SW_group.data, 'UniformOutput', 0);
     
-    SW_group = SW_group.unpackDim(2);
+    SW_group = SW_group.unpackDim(recording_pack_dim);
     
     SW_group.axis(findaxis(SW_group,'Recording')).values = ... % SW_xPlt = SW_xPlt.importAxisValues(SW_xPlt, 'Recording', {folders(groups_plotted{group}{2})});
         folders(groups_plotted{group}{2});
-
-    if ~isempty(SW_group.findaxis('Window_Dim_1'))
-        %% Plotting if there are sliding windows.
-        
-        close('all')
-        
-        %% % % False color image of all windows by recording. % % %
-        
-        SW_WindowsPacked = squeeze(SW_group.packDim('Window_Dim_1', 2));
-        
-        SW_WindowsPacked.getaxisinfo
-        
-        function_handles = {@xp_subplot_grid_adaptive,@xp_matrix_imagesc};
-        function_arguments = {{{'Recording', 'Period', 'Channel'}},{}};
-        dimensions = {1:length(size(SW_WindowsPacked)),0};
-        recursivePlot(SW_WindowsPacked,function_handles,dimensions,function_arguments);
-        
-        save_all_figs([group_name, '_windows'])
-        
-        close('all')
-        
-        % waitforbuttonpress
-        
-        %% % % Comparison over windows, plotted by recording. % % %
-        
-        function_handles = {@xp_subplot_grid_adaptive,@xp_compare_2D};
-        function_arguments = {{},{@ttest, significance}};
-        dimensions = {{'Recording', 'Channel'},{'Period'}};
-        recursivePlot(SW_WindowsPacked,function_handles,dimensions,function_arguments);
-        
-        save_all_figs([group_name, '_windows_compared_by_subject'])
-        
-        close('all')
-        
-        % waitforbuttonpress
-        
-        %% % % Comparison of mean across windows, over recordings. % % %
-        
-        SW_WindowMean = SW_WindowsPacked;
-        
-        SW_WindowMean.data = cellfun(@(x) mean(x, 2), SW_WindowMean.data, 'UniformOutput', 0);
-        
-        SW_WindowMean = squeeze(SW_WindowMean.packDim('Recording', 2));
-        
-        SW_WindowMean.getaxisinfo
-        
-        dimensions = {{'Channel'},{'Period'}};
-        recursivePlot(SW_WindowMean,function_handles,dimensions,function_arguments);
-        
-        save_all_figs([group_name, '_windows_compared'])
+    
+    if ~any(size_dim2(:) > 1)
+    
+        if ~isempty(SW_group.findaxis('Window_Dim_1'))
+            %% Plotting if there are sliding windows.
+            
+            close('all')
+            
+            SW_WindowMean = mean_over_axis(SW_group, 'Window_Dim_1');
+            
+            %% % % False color image of all windows by recording. % % %
+            
+            SW_WindowsPacked = squeeze(SW_group.packDim('Window_Dim_1', 2));
+            
+            SW_WindowsPacked.getaxisinfo
+            
+            function_handles = {@xp_tight_subplot_adaptive,@xp_matrix_imagesc};
+            function_arguments = {{{'Recording', 'Period', 'Channel'}},{}};
+            dimensions = {1:length(size(SW_WindowsPacked)),0};
+            recursivePlot(SW_WindowsPacked,function_handles,dimensions,function_arguments);
+            
+            save_all_figs([group_name, '_windows'])
+            
+            close('all')
+            
+            % waitforbuttonpress
+            
+            %% % % Pre-post comparison over windows, plotted by recording. % % %
+            
+            function_handles = {@xp_tight_subplot_adaptive,@xp_compare_2D};
+            function_arguments = {{},{@ttest, significance, [], 1}};
+            dimensions = {{'Recording', 'Channel'},{'Period'}};
+            recursivePlot(SW_WindowsPacked,function_handles,dimensions,function_arguments);
+            
+            save_all_figs([group_name, '_windows_by_subject_compared_p', num2str(significance)])
+            
+            close('all')
+            
+            % waitforbuttonpress
+            
+            %% % % Pre-post comparison of mean across windows, over recordings. % % %
+            
+            SW_WindowMean = squeeze(SW_WindowMean.packDim('Recording', 2));
+            
+            SW_WindowMean = norm_axis_by_value(SW_WindowMean, 'Period', 'pre');
+            
+            SW_WindowMean.getaxisinfo
+            
+            dimensions = {{'Channel'},{'Period'}};
+            recursivePlot(SW_WindowMean,function_handles,dimensions,function_arguments);
+            
+            save_all_figs([group_name, '_windows_compared_p', num2str(significance)])
+            
+        else
+            %% Plotting if there are no sliding windows.
+            
+            close('all')
+            
+            %% % % Plots by recording. % % %
+            
+            SW_RecordingsPacked = squeeze(SW_group.packDim('Recording', 2));
+            
+            function_handles = {@xp_tight_subplot_adaptive,@xp_matrix_basicplot};
+            function_arguments = {{{'Period', 'Channel'}},{}};
+            dimensions = {1:length(size(SW_RecordingsPacked)),0};
+            recursivePlot(SW_RecordingsPacked,function_handles,dimensions,function_arguments);
+            
+            save_all_figs([group_name, '_recordings'])
+            
+            close('all')
+            
+            % waitforbuttonpress
+            
+            %% % % Pre-post comparisons over recordings. % % %
+            
+            function_handles = {@xp_tight_subplot_adaptive,@xp_compare_2D};
+            function_arguments = {{},{@ttest, significance, [], 1}};
+            dimensions = {{'Channel'},{'Period'}};
+            recursivePlot(SW_RecordingsPacked,function_handles,dimensions,function_arguments);
+            
+            save_all_figs([group_name, '_recordings_compared_p', num2str(significance)])
+            
+        end
         
     else
-        %% Plotting if there are no sliding windows.
         
-        close('all')
-        
-        %% % % Plots by recording. % % %
-        
-        SW_RecordingsPacked = squeeze(SW_group.packDim('Recording', 2));
-        
-        function_handles = {@xp_subplot_grid_adaptive,@xp_matrix_basicplot};
-        function_arguments = {{{'Period', 'Channel'}},{}};
-        dimensions = {1:length(size(SW_RecordingsPacked)),0};
-        recursivePlot(SW_RecordingsPacked,function_handles,dimensions,function_arguments);
-        
-        save_all_figs([group_name, '_recordings'])
-        
-        close('all')
-        
-        % waitforbuttonpress
-        
-        %% % % Pre-post comparisons over recordings. % % %
-        
-        function_handles = {@xp_subplot_grid_adaptive,@xp_compare_2D};
-        function_arguments = {{},{@ttest, significance}};
-        dimensions = {{'Channel'},{'Period'}};
-        recursivePlot(SW_RecordingsPacked,function_handles,dimensions,function_arguments);
-        
-        save_all_figs([group_name, '_recordings_compared'])
+        if ~isempty(SW_group.findaxis('Window_Dim_1'))
+            %% Plotting if there are sliding windows.
+            
+            close('all')
+            
+            %% % % Mean over windows, plotted by recording. % % %
+            
+            SW_WindowMean = mean_over_axis(SW_group, 'Window_Dim_1');
+            
+            SW_WindowMean.getaxisinfo
+            
+            function_handles = {@xp_tight_subplot_adaptive,@xp_matrix_imagesc};
+            function_arguments = {{[], [], [], 'row'},{1,1}};
+            dimensions = {{'Recording', 'Period', 'Channel'},{}};
+            recursivePlot(SW_WindowMean,function_handles,dimensions,function_arguments);
+            
+            save_all_figs([group_name, '_window_mean_by_subject'])
+            
+            close('all')
+            
+            %% % % Pre-post comparison over windows, plotted by recording. % % %
+            
+            SW_WindowsPacked = packDim(SW_group, 'Window_Dim_1');
+            
+            SW_WindowsPacked.getaxisinfo
+            
+            function_handles = {@xp_tight_subplot_adaptive,@xp_compare_3D};
+            function_arguments = {{},{@ttest,significance,1,'pcolor'}};
+            dimensions = {{'Recording', 'Channel'},{'Period'}};
+            recursivePlot(SW_WindowsPacked,function_handles,dimensions,function_arguments)
+            
+            save_all_figs([group_name, '_by_subject_compared_p', num2str(significance)])
+            
+            close('all')
+            
+            %% % % Mean across recordings. % % %
+            
+            SW_RecordingMean = mean_over_axis(SW_WindowMean, 'Recording');
+            
+            SW_RecordingMean.getaxisinfo
+            
+            function_handles = {@xp_tight_subplot_adaptive,@xp_matrix_imagesc};
+            function_arguments = {{[], [], [], 'row'},{1,1}};
+            dimensions = {{'Channel','Period'},{}};
+            recursivePlot(SW_RecordingMean,function_handles,dimensions,function_arguments);
+            
+            save_all_figs([group_name, '_window_mean'])
+            
+            close('all')
+            
+            %% % % Pre-post comparison over windows, plotted by recording. % % %
+            
+            SW_RecordingsPacked = packDim(SW_WindowMean, 'Recording');
+            
+            SW_RecordingsPacked.getaxisinfo
+            
+            function_handles = {@xp_tight_subplot_adaptive,@xp_compare_3D};
+            function_arguments = {{},{@ttest,significance,1,'pcolor'}};
+            dimensions = {{'Channel'},{'Period'}};
+            recursivePlot(SW_RecordingsPacked,function_handles,dimensions,function_arguments)
+            
+            save_all_figs([group_name, '_compared_p', num2str(significance)])
+            
+            close('all')
+            
+        else
+            %% Plotting if there are no sliding windows.
+            
+            close('all')
+            
+            %% % % Plots by recording. % % %
+            
+            function_handles = {@xp_tight_subplot_adaptive,@xp_matrix_imagesc};
+            function_arguments = {{[], [], [], 'row'},{1,1}};
+            dimensions = {{'Recording', 'Period', 'Channel'},{}};
+            recursivePlot(SW_group,function_handles,dimensions,function_arguments);
+            
+            save_all_figs([group_name, '_by_subject'])
+            
+            close('all')
+            
+            %% % % Pre-post comparisons over recordings. % % %
+            
+            SW_RecordingMean = mean_over_axis(SW_group, 'Recording');
+            
+            SW_RecordingMean.getaxisinfo
+            
+            dimensions = {{'Channel','Period'},{}};
+            recursivePlot(SW_RecordingsPacked,function_handles,dimensions,function_arguments);
+            
+            save_all_figs([group_name, '_subject_mean'])
+            
+        end
         
     end
     
 end
-
-end
-
-function x = empty_decimate(x, factor)
-    
-    if ~isempty(x)
-        
-        x = decimate(x, factor);
-        
-    end
 
 end
